a34552af6534224f754f8c154cb8f22527f2ed5c
Heini-Maria Kotipelto <115211431+Heini-Maria@users.noreply.github.com>
2023-02-02 15:15:19 +0100

    cleaned Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,48 +1,54 @@
-import { StyleSheet, View, Text, Image } from "react-native";
-import Constants  from "expo-constants";
-
-export default function Header() {
-    return(
-        <View style={styles.header}>
-            <Image style={styles.logo} source={require('../../assets/Images/logo.png')}/>
-            <Text style={styles.text}>Repeat</Text>
-        </View>
-    )
-}
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Image,
+} from 'react-native';
+import Constants from 'expo-constants';
 
 const styles = StyleSheet.create({
-    header: {
-        flex: 3,
-        width: '100%',
-        alignItems: 'center',
-        justifyContent: 'flex-start',
-        backgroundColor: '#9CBFDF',
-        paddingTop: Constants.StatusBarHeight ,
-        paddingBottom: 10,
-        zIndex: 5,
-        flexWrap: 'wrap',
-        flexDirection: 'row',
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 2,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 3.84,
-        elevation: 5,   
-        },
-    text: {
-        fontSize: 30,
-        fontFamily: 'Shrikhand',
-        color: '#463C33',
-        marginTop: 10,
-        position: 'relative',
-        left: '90%',
+  header: {
+    flex: 3,
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    backgroundColor: '#9CBFDF',
+    paddingTop: Constants.StatusBarHeight ,
+    paddingBottom: 10,
+    zIndex: 5,
+    flexWrap: 'wrap',
+    flexDirection: 'row',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
     },
-    logo: {
-        marginTop: 100,
-        width: 35,
-        height: 35,
-        marginLeft: 36
-    }
-})
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  text: {
+    fontSize: 30,
+    fontFamily: 'Shrikhand',
+    color: '#463C33',
+    marginTop: 10,
+    position: 'relative',
+    left: '90%',
+  },
+  logo: {
+    marginTop: 100,
+    width: 35,
+    height: 35,
+    marginLeft: 36,
+  },
+});
+
+export default function Header() {
+  return (
+    <View style={styles.header}>
+      <Image style={styles.logo} source={require('../../assets/Images/logo.png')}/>
+      <Text style={styles.text}>Repeat</Text>
+    </View>
+  );
+}
